60441c0b49acb2d148a21128dba92cf8d444e17e
Davide Girotto <dgirotto@goresponsa.com>
2021-11-23 16:21:09 +0100

    Added danieli demo to index

---
+++ GetConnected _ Agile Transformation - Atlassian Platinum Partner_files/iubenda_cs.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var e="96e97d66b0ea45552958bbe0bd63f813",t=document.createElement("script"),c=document.getElementsByTagName("script")[0];t.src="//cdn.iubenda.com/cookie_solution/iubenda_cs/core-"+e+".js",c.parentNode.insertBefore(t,c)}();
