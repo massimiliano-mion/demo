8a56ea5f872ef8502206cdc92193dac4e9a1c07c
Andrea Rinaldi <andrea.rinaldi@euris.it>
2022-06-20 17:08:42 +0200

    bnp proactive form sample

---
+++ bnp/form/RichiestaSubentro_files/ScriptResource(5).axd
@@ -0,0 +1,7 @@
+﻿// Name:        Localization.Resources.it.debug.js
+// Assembly:    AjaxControlToolkit
+// Version:     18.1.1.0
+// FileVersion: 18.1.1.0
+Sys.Extended.UI.Localization["it"] = {
+    "Calendar_Today": "Oggi: {0}"
+};
---
+++ bnp/form/static/lhc.js
@@ -0,0 +1,31 @@
+if (typeof lhc_var === 'undefined') {
+    window.lhc_var = {};
+};
+lhc_var.customerCode = 'MGFWQkpyamhxaWFKSWsvU2JrNVI0TWpjTUMrQnlzdGpVb1QxQ2k4VEZkdGtXYWdWcDBUbUYzcFpiUE95TEVhSDZMd1FibGg4Y0VJWTJoVVRVLy8vdnc9PQ==';
+lhc_var.origin = 'ilmioleasing';
+lhc_var.decision_tree = 'post-login';
+
+var LHC_API = LHC_API || {};
+LHC_API.args = {
+    mode: 'widget',
+    lang: 'ita/',
+    lhc_base_url: '//helpchat.goresponsa.com/bnp_dev/index.php/',
+    wheight: 450,
+    wwidth: 350,
+    pheight: 520,
+    pwidth: 500,
+    leaveamessage: true,
+    department: [1],
+    theme: 9,
+    check_messages: false
+};
+(function () {
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.co
+    var date = new Date();
+    po.src = '//helpchat.goresponsa.com/bnp_dev/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+})();
